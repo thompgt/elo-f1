0c82b6dc311d234a8585701d1913cd0c8dd09bd0
Thomas Pequegnot <thomas.pequegnot04@gmail.com>
2026-09-01 15:11:24 -0400

    Broaden CORS to any localhost port for dev flexibility

--- src/elo_f1/api/main.py
+++ src/elo_f1/api/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="elo-f1 API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
     allow_methods=["GET"],
     allow_headers=["*"],
 )
